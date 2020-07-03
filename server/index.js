54ba607d663c7bb5c5a4632ed9b1b9a8bea59d17
Alex Rueb <rueba@oregonstate.edu>
2020-07-02 23:30:21 -0500

    added backend starter files

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+require("./startup/db")();
+require("./startup/routes")(app);
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function () {
+  console.log("GroceryGram API listening on port 3001!");
+});
+
+module.exports = server;
