e62961f81ca8280b46957ace455283fcf1e0183d
Alex Rueb <rueba@oregonstate.edu>
2020-07-02 22:38:52 -0500

    added client starter files

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./components/navbar";
+import ShoppingList from "./components/shoppingList";
+import MyRecipes from "./components/myRecipes";
+import ExploreRecipes from "./components/exploreRecipes";
+import NotFound from "./components/notFound";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    // get the current user here
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <Navbar />
+
+        <main className="container">
+          <Switch>
+            {/* <Route path="/login" component={LoginForm} /> */}
+            {/* <Route path="/logout" component={Logout} /> */}
+            {/* <Route path="/register" component={RegisterForm} /> */}
+            <Route path="/my-recipes" component={MyRecipes} />
+            {/* <Route path="/my-recipes/:id" component={RecipeForm} /> */}
+            <Route path="/explore-recipes" component={ExploreRecipes} />
+            {/* <Route path="/recipes/:id" component={RecipeDetail} /> */}
+            <Route path="/shopping-list" component={ShoppingList} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect exact from="/" to="/shopping-list" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/myRecipes.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class MyRecipes extends Component {
+  state = {};
+  render() {
+    return <h1>My Recipes</h1>;
+  }
+}
+
+export default MyRecipes;
