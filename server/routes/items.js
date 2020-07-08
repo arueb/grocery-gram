318b6fd536345232957de70348def2e707590816
David Coopersmith <dcoopersmith@mvusd.net>
2020-07-08 07:37:05 -0700

    add item.js with itemSchema, and items.js with GET /api/items to get all items

---
+++ server/routes/items.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { Item, validate } = require("../models/item");
+
+router.get("/", (req, res) => {
+  console.log("hey it's items!");
+  res.send({ hello: "items world" });
+});
+
+// router.post("/", async (req, res) => {
+//   const { error } = validate(req.body);
+//   if (error) return res.status(400).send(error.details[0].message);
+
+//   let user = await User.findOne({ email: req.body.email });
+//   if (user)
+//     return res
+//       .status(400)
+//       .send("A user with this email address is already registered.");
+
+//   user = new User(_.pick(req.body, ["username", "email", "password"]));
+//   const salt = await bcrypt.genSalt(10);
+//   user.password = await bcrypt.hash(user.password, salt);
+//   await user.save();
+
+//   const token = user.generateAuthToken();
+
+//   res
+//     .header("x-auth-token", token)
+//     .header("access-control-expose-headers", "x-auth-token")
+//     .send(_.pick(user, ["_id", "name", "email"]));
+// });
+
+// router.get("/", (req, res) => {
+//   console.log("here i am");
+//   res.send({ hello: "world" });
+// });
+
+module.exports = router;
