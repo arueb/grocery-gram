f2293506c7217deb8fb10e64e06122e9a63ef858
David Coopersmith <dcoopersmith@mvusd.net>
2020-07-09 12:07:46 -0700

    finished create item endpoint

--- server/routes/items.js
+++ server/routes/items.js
@@ -4,6 +4,22 @@ const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { Item, validate } = require("../models/item");
 
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let item = await Item.findOne({ name: req.body.name });
+  if (item)
+    return res
+      .status(400)
+      .send("An item with this name already exists in DB.");
+
+  item = new Item(_.pick(req.body, ["name", "category", "price"]));
+  await item.save();
+
+  res.send('coming from POST /items');
+});
+
 router.get("/", async (req, res) => {
   try {
     const items = await Item.find().sort('name');
@@ -25,33 +41,5 @@ router.get("/:id", async (req, res) => {
     res.status(500).send("Something failed.");
   }
 });
-
-// router.post("/", async (req, res) => {
-//   const { error } = validate(req.body);
-//   if (error) return res.status(400).send(error.details[0].message);
-
-//   let user = await User.findOne({ email: req.body.email });
-//   if (user)
-//     return res
-//       .status(400)
-//       .send("A user with this email address is already registered.");
-
-//   user = new User(_.pick(req.body, ["username", "email", "password"]));
-//   const salt = await bcrypt.genSalt(10);
-//   user.password = await bcrypt.hash(user.password, salt);
-//   await user.save();
-
-//   const token = user.generateAuthToken();
-
-//   res
-//     .header("x-auth-token", token)
-//     .header("access-control-expose-headers", "x-auth-token")
-//     .send(_.pick(user, ["_id", "name", "email"]));
-// });
-
-// router.get("/", (req, res) => {
-//   console.log("here i am");
-//   res.send({ hello: "world" });
-// });
 
 module.exports = router;
