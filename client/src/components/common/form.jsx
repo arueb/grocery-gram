8bfa2600d974e9c4edb4c70e8b706ba00b977bde
Alex Rueb <rueba@oregonstate.edu>
2020-07-12 13:04:24 -0500

    Started add ingredients from.  Needs further refactoring.

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./input";
+import Select from "./select";
 
 class Form extends Component {
   state = {
@@ -45,6 +46,19 @@ class Form extends Component {
     this.setState({ data, errors });
   };
 
+  handleChangeMultiRow = async ({ currentTarget: input }, row) => {
+    const errors = { ...this.state.errors };
+    // const errorMessage = this.validateProperty(input);
+
+    // if (!errorMessage) delete errors[input.name];
+    console.log("row", row);
+    console.log("input", input);
+    const data = [...this.state.data];
+    console.log("data", data);
+    data[row][input.name] = input.value;
+    this.setState({ data, errors });
+  };
+
   renderButton(label) {
     console.log(this.validate());
     return <button className="btn btn-primary">{label}</button>;
@@ -59,6 +73,48 @@ class Form extends Component {
         label={label}
         value={data[name]}
         onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+
+  renderMultiRowInput(name, label, row, type = "text") {
+    const { data, errors } = this.state;
+    return (
+      <Input
+        type={type}
+        name={name}
+        label={label}
+        value={data[row][name]}
+        onChange={(e) => this.handleChangeMultiRow(e, row)}
+        error={errors[name]}
+      />
+    );
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+    return (
+      <Select
+        name={name}
+        label={label}
+        value={data[name]}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+
+  renderMultiRowSelect(name, label, row, options) {
+    const { data, errors } = this.state;
+    return (
+      <Select
+        name={name}
+        label={label}
+        value={data[name]}
+        options={options}
+        onChange={(e) => this.handleChangeMultiRow(e, row)}
         error={errors[name]}
       />
     );
