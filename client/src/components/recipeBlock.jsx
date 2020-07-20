abd83ee9347cef06fa6a4986e1de8951503dd966
Brooks Przybylek <brooks.przybylek@gmail.com>
2020-07-20 14:57:20 -0500

    static data shown on card

--- client/src/components/recipeBlock.jsx
+++ client/src/components/recipeBlock.jsx
@@ -6,7 +6,15 @@ class RecipeBlock extends Component {
     return (
       <React.Fragment>
         {console.log("hey from RecipeBlock")}
-        <p>{this.props._id}</p>
+        {console.log(this.props.recipe)}
+        <div className="card" style={{ width: '18rem' }}>
+          <img className="card-img-top" src={this.props.recipe.images[0].thumbUrl} alt="the thing" />
+          <div className="card-body">
+            <h5 className="card-title">{this.props.recipe.title}</h5>
+            <p className="card-text">{this.props.recipe.instructions}</p>
+            <a href={process.env.REACT_APP_API_URL + '/recipes/' + this.props.recipe._id} class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </React.Fragment>
     )
   }
