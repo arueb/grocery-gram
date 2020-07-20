19f064b8498012162a0078f3b2c14d3e3635cb95
David Coopersmith <dcoopersmith@mvusd.net>
2020-07-19 17:41:10 -0700

    begin My Recipes top portion

--- client/src/components/myRecipes.jsx
+++ client/src/components/myRecipes.jsx
@@ -3,7 +3,18 @@ import React, { Component } from "react";
 class MyRecipes extends Component {
   state = {};
   render() {
-    return <h1>My Recipes</h1>;
+    return (
+    <React.Fragment>
+      <div className="row sl-page-heading">
+        <div className="col-md-9">
+          <h2>My Recipes</h2>
+        </div>
+        <div className="col-md">
+          <button className="">+ New Recipe</button>
+        </div>
+      </div>
+    </React.Fragment>
+    )
   }
 }
 
