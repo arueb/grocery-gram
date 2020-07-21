f241717ad2c1a666b2d00ceea1decd69387c8c5b
Brooks Przybylek <brooks.przybylek@gmail.com>
2020-07-20 23:05:22 -0500

    fixed link in recipeBlocks

--- client/src/components/recipeBlock.jsx
+++ client/src/components/recipeBlock.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-//import Link from 'react-router-dom'
+import { Link } from 'react-router-dom'
 //import { FaStarHalfAlt, FaStar, FaRegStar, FaHeart, FaRegHeart } from "react-icons/fa";
 //import StarRating from "./common/starRating";
 import AvgStarRating from "./common/avgStarRating";
@@ -22,16 +22,17 @@ class RecipeBlock extends Component {
   render(props) {
     return (
       <React.Fragment>
-        <div className="card" style={{ width: '200px' }}>
-          {this.placeholderOrImage()}          
-          <div className="card-body bg-secondary text-white">
-            <h5 className="card-title">{this.props.recipe.title}</h5>
-            <p className="card-text">{this.props.recipe.instructions}</p>
-            <p className="card-text">{"by " + this.props.recipe.userId}</p>
-            <AvgStarRating avgRating={this.props.recipe.avgRating} numReviews={this.props.recipe.numReviews} starSize={20} />
-            <a href={window.location.pathname + '/' + this.props.recipe._id} className="stretched-link">Here</a>
+        <Link to={'/my-recipes/' + this.props.recipe._id}>
+          <div className="card" style={{ width: '200px' }}>
+            {this.placeholderOrImage()}
+            <div className="card-body bg-secondary text-white">
+              <h5 className="card-title">{this.props.recipe.title}</h5>
+              <p className="card-text">{this.props.recipe.instructions}</p>
+              <p className="card-text">{"by " + this.props.recipe.userId}</p>
+              <AvgStarRating avgRating={this.props.recipe.avgRating} numReviews={this.props.recipe.numReviews} starSize={20} />
+            </div>
           </div>
-        </div>
+        </Link>
       </React.Fragment>
     )
   }
