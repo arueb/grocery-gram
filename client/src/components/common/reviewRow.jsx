656742efe5af2302e4509003dfd711b9b834b7e8
David Coopersmith <dcoopersmith@mvusd.net>
2020-07-28 17:53:34 -0700

    working on formatting the bottom portion of RDP

--- client/src/components/common/reviewRow.jsx
+++ client/src/components/common/reviewRow.jsx
@@ -16,26 +16,55 @@ class ReviewRow extends Component {
     }
 
     return (
-      <div className="row mt-5">
-        <div className="col-1">
-          <img className="text-center rounded-circle" src="https://picsum.photos/20" alt="lorem" />
-        </div>
-        <div className="col-11">
-          <div className="row">
-            <div className="col-6 align-left">
-              <h4>{this.props.username}</h4>
-              <AvgStarRating avgRating={this.props.rating} starSize={starSize} />
-            </div>
-            <div className="col-6 align-right ">
-              <p>{this.prettyDate()}</p>
-            </div>
+      <React.Fragment>
+        <div className="row mt-5">
+          <div className="col-">
+            <img
+              className="text-center rounded-circle"
+              src="https://picsum.photos/20"
+              alt="lorem"
+            />
           </div>
-          <div className="row">
-            <p>{this.props.comments}</p>
+          <div className="col-">
+            <span className="review-user">@{this.props.username}</span>
+          </div>
+          <div className="ml-auto">{this.prettyDate()}</div>
+        </div>
+        <div className="row">
+          <div className="col-"></div>
+          <div className="col-auto ml-3">
+            <AvgStarRating avgRating={this.props.rating} starSize={starSize} />
           </div>
         </div>
-      </div>
-    )
+        <div className="row">
+          <div className="col-"></div>
+          <div className="col-auto ml-3">
+            {this.props.comments}
+          </div>
+          {/* <p>{this.props.comments}</p> */}
+        </div>
+      </React.Fragment>
+
+      // <div className="row mt-5">
+      //   <div className="col-1">
+      //     <img className="text-center rounded-circle" src="https://picsum.photos/20" alt="lorem" />
+      //   </div>
+      //   <div className="col-11">
+      //     <div className="row">
+      //       <div className="col-9 align-left">
+      //         <h6>@{this.props.username}</h6>
+      //         <AvgStarRating avgRating={this.props.rating} starSize={starSize} />
+      //       </div>
+      //       <div className="align-right ">
+      //         <p>{this.prettyDate()}</p>
+      //       </div>
+      //     </div>
+      //     <div className="row">
+      //       <p>{this.props.comments}</p>
+      //     </div>
+      //   </div>
+      // </div>
+    );
   }
 }
 
