f403346f9665bdfde6efddcf96069c660c5ee14f
David Coopersmith <dcoopersmith@mvusd.net>
2020-07-29 09:19:08 -0700

    cleanup

--- client/src/components/common/reviewRow.jsx
+++ client/src/components/common/reviewRow.jsx
@@ -43,26 +43,6 @@ class ReviewRow extends Component {
           <div className="col-"></div>
         </div>
       </React.Fragment>
-
-      // <div className="row mt-5">
-      //   <div className="col-1">
-      //     <img className="text-center rounded-circle" src="https://picsum.photos/20" alt="lorem" />
-      //   </div>
-      //   <div className="col-11">
-      //     <div className="row">
-      //       <div className="col-9 align-left">
-      //         <h6>@{this.props.username}</h6>
-      //         <AvgStarRating avgRating={this.props.rating} starSize={starSize} />
-      //       </div>
-      //       <div className="align-right ">
-      //         <p>{this.prettyDate()}</p>
-      //       </div>
-      //     </div>
-      //     <div className="row">
-      //       <p>{this.props.comments}</p>
-      //     </div>
-      //   </div>
-      // </div>
     );
   }
 }
