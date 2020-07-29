93c0c27f20462fc26e06602fe957f9ba650abe88
David Coopersmith <dcoopersmith@mvusd.net>
2020-07-29 12:32:21 -0700

    used alex's styling for previous reviews, no grid, abs/rel positioning

--- client/src/components/common/reviewRow.jsx
+++ client/src/components/common/reviewRow.jsx
@@ -17,7 +17,21 @@ class ReviewRow extends Component {
 
     return (
       <React.Fragment>
-        <div className="row mt-5">
+        <div className="review-row">
+          <img
+            className="text-center rounded-circle"
+            src="https://picsum.photos/20"
+            alt="lorem"
+          />
+          <p className="review-username">@{this.props.username}</p>
+          <AvgStarRating
+            avgRating={this.props.rating}
+            starSize={starSize}
+          />
+          <span className="review-date">{this.prettyDate()}</span>
+          <p>{this.props.comments}</p>
+        </div>
+        {/* <div className="row mt-5">
           <div className="col">
             <img
               className="text-center rounded-circle"
@@ -41,7 +55,7 @@ class ReviewRow extends Component {
           <div className="col-"></div>
           <div className="col-auto ml-3">{this.props.comments}</div>
           <div className="col-"></div>
-        </div>
+        </div> */}
       </React.Fragment>
     );
   }
