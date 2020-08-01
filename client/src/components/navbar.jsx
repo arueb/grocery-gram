6ad020ec55eeb8403efc4e366ece01c7a31698fb
David Coopersmith <dcoopersmith@mvusd.net>
2020-08-01 14:18:42 -0700

    fixed issue with collapse hamburger menu on navbar selection

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -18,22 +18,43 @@ const Navbar = ({ user }) => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div
-          className="navbar-collapse collapse justify-content-stretch"
+          className="collapse navbar-collapse justify-content-stretch"
           id="navbar6"
         >
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/shopping-list">
+            <li
+              className="nav-item"
+              data-toggle="collapse"
+              data-target=".navbar-collapse.show"
+            >
+              <NavLink
+                to="/shopping-list"
+                className="nav-link"              
+              >
                 Shopping List <span className="sr-only">Home</span>
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/my-recipes">
+            <li
+              className="nav-item"
+              data-toggle="collapse"
+              data-target=".navbar-collapse.show"
+            >
+              <NavLink
+                to="/my-recipes"
+                className="nav-link"
+              >
                 My Recipes
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/explore-recipes">
+            <li
+              className="nav-item"
+              data-toggle="collapse"
+              data-target=".navbar-collapse.show"
+            >
+              <NavLink
+                to="/explore-recipes"
+                className="nav-link"
+              >
                 Explore Recipes
               </NavLink>
             </li>
@@ -41,12 +62,20 @@ const Navbar = ({ user }) => {
           <ul className="navbar-nav ml-auto">
             {!user && (
               <React.Fragment>
-                <li className="nav-item">
+                <li
+                  className="nav-item"
+                  data-toggle="collapse"
+                  data-target=".navbar-collapse.show"
+                >
                   <NavLink className="nav-link" to="/register">
                     Register
                   </NavLink>
                 </li>
-                <li className="nav-item">
+                <li
+                  className="nav-item"
+                  data-toggle="collapse"
+                  data-target=".navbar-collapse.show"
+                >
                   <NavLink className="nav-link" to="/login">
                     Login
                   </NavLink>
@@ -64,21 +93,23 @@ const Navbar = ({ user }) => {
                     aria-haspopup="true"
                     aria-expanded="false"
                   >
-                    {user.profileImageUrl ?
+                    {user.profileImageUrl ? (
                       <img
                         className="text-center rounded-circle mr-2"
                         src={user.profileImageUrl}
                         alt="User Profile"
                         style={{ width: "30px", height: "30px" }}
                       />
-                      :
+                    ) : (
                       <FaUserCircle className="mr-1" size={20} />
-                    }
+                    )}
                     {user.username}
                   </Link>
                   <div
                     className="dropdown-menu mt-3"
                     aria-labelledby="navbarDropdownMenuLink"
+                    data-toggle="collapse"
+                    data-target=".navbar-collapse.show"
                   >
                     <NavLink className="dropdown-item" to="/profile">
                       <FaUser className="mr-1" />
