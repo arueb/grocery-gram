c874c2749f8191801cea47d71510d1f5ac5b1daf
David Coopersmith <dcoopersmith@mvusd.net>
2020-08-06 10:23:55 -0700

    finish adding colors from pantone

--- client/src/services/itemService.jsx
+++ client/src/services/itemService.jsx
@@ -16,12 +16,11 @@ const categories0 = [
   { _id: 10, name: "Herbs/Spices", hexColor: "#81b14f" },
   { _id: 11, name: "Breads", hexColor: "#8faab3" },
   { _id: 12, name: "Alcohol", hexColor: "#d19c4c" },
-  { _id: 13, name: "Sweetener", hexColor: "#9a6642" },
-  { _id: 14, name: "Oils/Fats", hexColor: "#5d3b1a" },
-  { _id: 15, name: "Sauces/Soups/Gravies", hexColor: "#25963e" },
-  { _id: 16, name: "Grilling Products", hexColor: "#d57500" },
-  { _id: 17, name: "Pasta", hexColor: "#bf6b59" },
-  { _id: 18, name: "Other", hexColor: "#fd79a8" },
+  { _id: 13, name: "Oils/Fats", hexColor: "#5d3b1a" },
+  { _id: 14, name: "Sauces/Soups/Gravies", hexColor: "#25963e" },
+  { _id: 15, name: "Grilling Products", hexColor: "#d57500" },
+  { _id: 16, name: "Pasta", hexColor: "#bf6b59" },
+  { _id: 17, name: "Other", hexColor: "#fd79a8" },
 ];
 
 // pantone color scheme
@@ -29,7 +28,7 @@ const categories1 = [
   { _id: 1, name: "Fruit", hexColor: "#B0C964" },
   { _id: 2, name: "Meat/Poultry", hexColor: "#EEC843" },
   { _id: 3, name: "Grains/Cereals", hexColor: "#87D9C3" },
-  { _id: 4, name: "Baking Products", hexColor: "#BFBDBE" },
+  { _id: 4, name: "Baking Products", hexColor: "#204478" },
   { _id: 5, name: "Dairy", hexColor: "#BE9867" },
   { _id: 6, name: "Nuts", hexColor: "#A66E4B" },
   { _id: 7, name: "Vegetables", hexColor: "#477050" },
@@ -38,12 +37,11 @@ const categories1 = [
   { _id: 10, name: "Herbs/Spices", hexColor: "#FB7A36" },
   { _id: 11, name: "Breads", hexColor: "#D04035" },
   { _id: 12, name: "Alcohol", hexColor: "#017F7C" },
-  { _id: 13, name: "Sweetener", hexColor: "#9a6642" },
-  { _id: 14, name: "Oils/Fats", hexColor: "#5d3b1a" },
-  { _id: 15, name: "Sauces/Soups/Gravies", hexColor: "#25963e" },
-  { _id: 16, name: "Grilling Products", hexColor: "#d57500" },
-  { _id: 17, name: "Pasta", hexColor: "#bf6b59" },
-  { _id: 18, name: "Other", hexColor: "#fd79a8" },
+  { _id: 13, name: "Oils/Fats", hexColor: "#E27A53" },
+  { _id: 14, name: "Sauces/Soups/Gravies", hexColor: "#C71859" },
+  { _id: 15, name: "Grilling Products", hexColor: "#CF212B" },
+  { _id: 16, name: "Pasta", hexColor: "#BA6C58" },
+  { _id: 17, name: "Other", hexColor: "#7D5379" },
 ];
 
 
