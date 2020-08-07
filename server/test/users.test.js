9a1dcb1ae465dd7f6096140e50a98880e7db5cd0
Brooks Przybylek <brooks.przybylek@gmail.com>
2020-08-07 14:52:18 -0500

    Add tests to /users

--- server/test/users.test.js
+++ server/test/users.test.js
@@ -8,11 +8,46 @@ const { expect } = chai;
 // Reference: https://scotch.io/tutorials/test-a-node-restful-api-with-mocha-and-chai
 
 chai.use(chaiHttp);
-describe("GroceryGram Backend Server", function () {
+describe("GroceryGram Backend Tests", function () {
+
+  it("Post /auth - No Credentials", done => {
+    chai
+      .request(app)
+      .post("/api/auth")
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res).to.not.have.header("x-auth-token");
+        done();
+      })
+  });
+
+  it("Post /auth - Incorrect email", done => {
+    chai
+      .request(app)
+      .post("/api/auth")
+      .send({ email: "aint@no.email", password: "brooks" })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res).to.not.have.header("x-auth-token");
+        done();
+      })
+  });
+
+  it("Post /auth - Incorrect Password", done => {
+    chai
+      .request(app)
+      .post("/api/auth")
+      .send({ email: "bp@bp.com", password: "NoThatsNotMyPassword" })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res).to.not.have.header("x-auth-token");
+        done();
+      })
+  });
 
   let authToken = "";
 
-  it("Post /auth", done => {
+  it("Post /auth - Get Token", done => {
     chai
       .request(app)
       .post("/api/auth")
@@ -23,6 +58,17 @@ describe("GroceryGram Backend Server", function () {
         authToken = res.header["x-auth-token"];
         done();
       })
+  });
+
+  it("GET /users - No Auth", done => {
+    chai
+      .request(app)
+      .get("/api/users")
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.not.be.a('array');
+        done();
+      });
   });
 
   it("GET /users", done => {
